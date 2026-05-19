14323da92588f72a579bcb19a4248bae5ecf4d61
muzamilbaig123 <muzamilbaig24@gmail.com>
2026-05-19 10:40:57 -0700

    day 12 is completed

---
+++ Day11 18-May-26-Monday-JS/script.js
@@ -0,0 +1,50 @@
+// let a         This is declareation
+// let a = 10      // This is intizianation
+
+// a = 20
+
+// console.log(a) 
+
+
+
+// artihmetic operator
+let a = 10 
+let b = 8 
+
+console.log(a + b)  //18 
+console.log(a - b)  // 2
+console.log(a * b)  // 80
+console.log(a / b)  // 
+console.log(a % b)
+
+
+// not allowd vairble
+
+// 1a, $1, for
+
+// using camelCase
+// javaScript
+
+// .wanr .log. .error
+
+
+// DataTypes define nahi karna hota lakin hoty hain 
+// premetive 
+// number, string, boolean, begInt, Symbol = unique valu ke liye use hota hai agr same variable name ho , null, undefine
+
+// non - premtive
+// objects
+// Array,
+// Funcation
+
+
+let c;
+let cc = null;
+let d = 10n;
+console.log(typeof cc)   //undefine default value of variable
+
+
+
+alert()     //web api ki taraf sey milta hai
+confirm()
+prompt()
---
+++ Day12 19-May-26-Monday-JS/script.js
@@ -0,0 +1,125 @@
+// Topic Name Type Coretion
+
+// kisi datatype ki type forcfuly ya automatic change karna 
+// implicit khud sey change ho jai 
+// explicit forcfuly change karna 
+
+// let one = "20";
+// let two = "2";
+
+// implicit type convert ho jata hai 
+// console.log(one * two)  // forcfully type ku convret kar raha hai 
+
+// explicit   
+
+// let ten = "10";
+// let twenty = "20";   
+
+// let tenCov = Number(ten) 
+// console.log(typeof(tenCov))
+
+
+
+
+// Binaray Operators
+
+// Artihmetic Operator                          +, -, *, /, %
+// Increment and decrement Operator             a++ a--
+// Assigmnet Operator         
+// Comparsion Operator
+// Logical Operator
+// Ternory Opeator
+
+
+
+// Increment Pre and post
+// let inc = 8;
+
+// console.log(inc++)   //post  bad me value change ho jai gi  
+// console.log(++inc)   //pre   pheley value change ho jai gi
+
+
+
+
+// Assigmnet
+// let x = 2;
+// x = x + 11
+
+// x += 11 
+// console.log(x, "xx")
+
+
+
+
+// Comparsion
+// true and fales
+
+// ==         ,          >,         <,      === strickly check,      !=  not equal to      !==
+
+
+
+
+
+// Logical
+// ||           &&            
+
+
+
+
+
+// conditionals
+
+
+// if (){}    else{ }          agr,   magar
+
+
+
+
+
+
+
+// Task 1  averge
+
+// let student = prompt("Whats your name ?")
+// let MathMask = Number(prompt("Enter Math Mask ?"));
+// let English = Number(prompt("Enter English Mask ?"));
+// let Urdu = Number(prompt("Enter Urdu Mask ?"));
+
+// let finalCal = (MathMask + English + Urdu) / 3;
+
+// if(finalCal >= 80){
+//     console.log(student, "Waoo You Are A One")
+// }
+// else if(finalCal <= 80 && finalCal >= 55){
+//     console.log(student, "Waoo You Are B Grade")
+// }
+// else{
+//     console.log("You Are Fail")
+// }
+
+
+
+
+
+
+// Falesly value
+
+// 0, fales, (empty string), null, undefined, NaN
+
+
+// Truty Value
+
+
+
+
+// Turneery Operator
+// a ? "good" : "by"
+
+
+
+
+// swtich case, statmnet
+
+
+
+
